5d64f26a5c6d9ef016c1e44a752e283507970232
Gabriel Mandelli <gabriel-mandelli@hotmail.com>
2019-06-20 17:04:11 -0300

    add NewPost and add realtime with socket.io

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import api from './../services/api';
+import io from 'socket.io-client';
 
 import './Feed.css'
 
@@ -16,8 +17,36 @@ class Feed extends Component {
   };
 
   async componentDidMount() {
+
+    this.registerToSocket();
+
     const response = await api.get('posts');
+
     this.setState({ feed: response.data });
+  }
+
+  // Realtime sync
+  registerToSocket = () => {
+    const socket = io('http://localhost:3333');
+
+    // post, like
+    socket.on('post', newPost => {
+      this.setState({ feed: [newPost, ...this.state.feed] });
+    })
+
+    socket.on('like', likedPost => {
+      this.setState({
+        feed: this.state.feed.map(post =>
+          post._id === likedPost._id ? likedPost : post
+        )
+      })
+    })
+
+  }
+
+  handleLike = id => {
+    console.log(`Like no post id: ${id}`);
+    api.post(`/posts/${id}/like`);
   }
 
   render() {
@@ -25,7 +54,7 @@ class Feed extends Component {
       <section id='post-list'>
         {this.state.feed.map(post => (
 
-          <article key={post.id}>
+          <article key={post._id}>
 
             <header>
               <div className='user-info'>
@@ -39,7 +68,9 @@ class Feed extends Component {
 
             <footer>
               <div className='actions'>
-                <img src={like} alt="" />
+                <button type="button" onClick={() => this.handleLike(post._id)}>
+                  <img src={like} alt="" />
+                </button>
                 <img src={comment} alt="" />
                 <img src={send} alt="" />
               </div>
