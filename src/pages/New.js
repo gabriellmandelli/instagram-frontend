2f4a71c878b9f07cbb7f2b47f1a5313f5653e0cb
Gabriel Mandelli <gabriel-mandelli@hotmail.com>
2019-06-20 13:41:57 -0300

    init commit

---
+++ src/pages/New.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class New extends Component {
+  render() {
+    return (
+      <form id="new-post">
+        <input type="file"/>
+        <input 
+          type="text
+          
+        "/>
+      </form>
+    );
+  }
+}
+
+export default New;
